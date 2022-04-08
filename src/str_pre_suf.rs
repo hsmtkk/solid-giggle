7296c89bba1322f76edfce503bf6619de2edfd81
hsmtkk <hsmtkk@gmail.com>
2022-04-09 08:10:04 +0900

    longest string prefix and suffix

--- src/str_pre_suf.rs
+++ src/str_pre_suf.rs
@@ -1,7 +1,7 @@
 fn longest_prefix_suffix(s: &str) -> usize {
-    for i in (0..s.len()).rev() {
+    for i in (0..s.len() / 2 + 1).rev() {
         let prefix = &s[..i];
-        let suffix = &s[s.len()-i..];
+        let suffix = &s[s.len() - i..];
         if prefix == suffix {
             return i;
         }
@@ -16,22 +16,22 @@ mod tests {
     #[test]
     fn test_longest_prefix_suffix() {
         let test_cases = vec![
-            ("abcd",     0),
-        ("abcda":    1),
-        ("abcdabc":  3),
-        ("abcabc":   3),
-        ("abcabca":  1),
-        ("abcdabcc": 0),
-        ("aaaaa":    2),
-        ("aaaa":     2),
-        ("aaa":      1),
-        ("aa":       1),
-        ("a":        0),
-        ("acbacc":   0),
+            ("abcd", 0, "abcd"),
+            ("abcda", 1, "abcda"),
+            ("abcdabc", 3, "abcdabc"),
+            ("abcabc", 3, "abcabc"),
+            ("abcabca", 1, "abcabca"),
+            ("abcdabcc", 0, "abcdabcc"),
+            ("aaaaa", 2, "aaaaa"),
+            ("aaaa", 2, "aaaa"),
+            ("aaa", 1, "aaa"),
+            ("aa", 1, "aa"),
+            ("a", 0, "a"),
+            ("acbacc", 0, "acbacc"),
         ];
         for test_case in test_cases {
             let got = longest_prefix_suffix(test_case.0);
-            assert_eq!(test_case.1, got);
+            assert_eq!(test_case.1, got, "{}", test_case.2);
         }
     }
 }
